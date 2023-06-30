4b877d0ee8f2272334f73dcecad7b3837e1b7587
YoungTony1695 <baothanhnguyen1695@gmail.com>
2023-06-30 13:59:25 +0700

    `

---
+++ src/doanjavacobangithub/pxu/Github.java
@@ -0,0 +1,14 @@
+package doanjavacobangithub.pxu;
+
+public class Github {
+
+	public Github() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("XinChao");
+	}
+
+}
